4f52be62cbae34629b87f566831379fc5377041e
Michael Okiri <maestro92@live.co.uk>
2022-06-30 13:08:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,13 @@
 
 /**
  * main - Entry point
- 
  * Description: puts "Programming is like building a multilingual puzzle"
- 
  * Return: 0
- 
  */
-
 
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
